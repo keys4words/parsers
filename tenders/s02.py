39fe5d60ffb6c784e8f4e3e9d67d7399ce068c13
keys4words <keys4words@gmail.com>
2020-09-18 20:52:32 +0300

    add click_navigation to tender app

---
+++ tenders/s02.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+driver.get('https://techwithtim.net/')
+link = driver.find_element_link_text("Python Programming")
+link.click()
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.LINK_TEXT, "Beginner Python Tutorials"))
+    )
+    element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.ID, "sow-button-19310003"))
+    )
+    element.click()
+    driver.back()
+    driver.back()
+    driver.back()
+    driver.forward()
+
+finally:
+    driver.quit()
+
