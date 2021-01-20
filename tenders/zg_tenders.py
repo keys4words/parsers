166d9523525a0e63386956ec304f4b39bf426138
keys4words <keys4words@gmail.com>
2021-01-20 12:10:40 +0300

    add zg_department for inn

--- tenders/zg_tenders.py
+++ tenders/zg_tenders.py
@@ -4,14 +4,21 @@ from openpyxl import Workbook
 from datetime import datetime
 import yagmail
 
-from conf_113 import from_email, password, to_emails, bcc
+from config.conf_113 import from_email, password, to_emails, bcc
+from config.conf_104 import to_emails2
+from config.conf_129 import to_emails3
 
 
 BASE_URL = 'https://zakupki.gov.ru'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_PATH = 'db\\zg_113.db'
+DB_PATH = 'db\\zg_department.db'
+
 FILE_WITH_INNS = os.path.join(BASE_DIR, 'inn', 'zg_113.txt')
-FILE_WITH_KW = os.path.join(BASE_DIR, 'keywords', 'zg_113.txt')
+FILE_WITH_INNS_104 = os.path.join(BASE_DIR, 'inn', 'zg_104.txt')
+FILE_WITH_INNS_129 = os.path.join(BASE_DIR, 'inn', 'zg_129.txt')
+
+# FILE_WITH_KW = os.path.join(BASE_DIR, 'keywords', 'zg_113.txt')
+
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36', 'accept': '*/*'}
 
 
@@ -44,8 +51,8 @@ def save_tender(number, name, url, customer, customer_url, price, release_date, 
 
 
 def set_logger():
-    root_logger = logging.getLogger('zg_113')
-    handler = logging.FileHandler('logs\\zg_113.log', 'a', 'utf-8')
+    root_logger = logging.getLogger('zg_tenders')
+    handler = logging.FileHandler('logs\\zg_tenders.log', 'a', 'utf-8')
     formatter = logging.Formatter(
         '%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     handler.setFormatter(formatter)
@@ -67,7 +74,7 @@ def get_html(url, params=None):
 
 
 def parsing(inns):
-    root_logger = logging.getLogger('zg_113')
+    root_logger = logging.getLogger('zg_tenders')
     for inn in inns:
         page_num = 1
         url = f'https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString={inn}&morphology=on&search-filter=%D0%94%D0%B0%D1%82%D0%B5+%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D1%8F&pageNumber={page_num}&sortDirection=false&recordsPerPage=_50&showLotsInfoHidden=false&sortBy=UPDATE_DATE&fz44=on&fz223=on&af=on&currencyIdGeneral=-1'
@@ -101,8 +108,12 @@ def parsing(inns):
                             last_customer = customer
                             last_customer_url = customer_url
                     except AttributeError:
-                        customer = last_customer
-                        customer_url = last_customer_url
+                        try:
+                            customer = last_customer
+                            customer_url = last_customer_url
+                        except UnboundLocalError:
+                            customer = ''
+                            customer_url = ''
 
                     price = el.find('div', class_="price-block__value")
                     price = price.text.strip().replace('\n', '').replace('\xa0', '')
@@ -151,33 +162,34 @@ def parsing(inns):
     root_logger.info('='*36)
 
 
-def save_results(res):
+def save_results(res, fileprefix):
     wb = Workbook()
     ws = wb.active
-    headers = ['Номер', 'Объект закупки', 'Ссылка на тендер', 'Заказчик',
-               'Ссылка на Заказчика', 'Начальная цена', 'Размещено', 'Обновлено', 'Окончание подачи заявок']
+    headers = ['Размещено', 'Обновлено', 'Окончание подачи заявок', 'Номер', 'Объект закупки', 'Ссылка на тендер', 'Заказчик',
+               'Начальная цена' ]
     ws.append(headers)
     for tender_number, tender_info in res.items():
-        ws.append([tender_number, tender_info['name'], tender_info['url'], tender_info['customer'],
-                   tender_info['customer_url'], tender_info['price'], tender_info['release_date'], tender_info['refreshing_date'], tender_info['ending_date']])
+        ws.append([tender_info['release_date'], tender_info['refreshing_date'], tender_info['ending_date'], tender_number, tender_info['name'], tender_info['url'], tender_info['customer'],
+                   tender_info['price'] ])
 
-    ws.column_dimensions['A'].width = 30
-    ws.column_dimensions['B'].width = 100
-    ws.column_dimensions['D'].width = 80
-    ws.column_dimensions['F'].width = 30
-    ws.column_dimensions['G'].width = 30
-    ws.column_dimensions['H'].width = 30
+    ws.column_dimensions['A'].width = 20
+    ws.column_dimensions['B'].width = 20
+    ws.column_dimensions['C'].width = 20
+    ws.column_dimensions['D'].width = 30
+    ws.column_dimensions['E'].width = 100
+    ws.column_dimensions['G'].width = 80
+    ws.column_dimensions['H'].width = 20
 
     name = os.path.abspath(os.path.dirname(__file__))
     name = os.path.join(name, 'out', datetime.now().strftime("%d-%m-%Y_%H-%M"))
 
-    results_file_name = name + '_zg_113.xlsx'
+    results_file_name = name + fileprefix + '.xlsx'
     wb.save(results_file_name)
-    root_logger = logging.getLogger('zg_113')
+    root_logger = logging.getLogger('zg_tenders')
     root_logger.info(f'File {results_file_name} was successfully saved')
     return results_file_name
 
-def sending_email(filename, subject):
+def sending_email(filename, subject, to_emails):
     body = "Below you find file with tenders from zakupki.gov"
     contents = [
         body,
@@ -192,20 +204,29 @@ def sending_email(filename, subject):
         bcc=bcc,
         contents=contents,
     )
-    root_logger = logging.getLogger('zg_113')
+    root_logger = logging.getLogger('zg_tenders')
     root_logger.info(f'File was sended to {to_emails}, blind copy: {bcc}')
 
 
 # main thread
+# interation for 113
 res = dict()
-# res = dict()
 
 set_logger()
 parsing(get_inns(FILE_WITH_INNS))
 # print(res)
-sending_email(save_results(res), 'zakupki-gov by INN')
-# sending_email(save_results(res_kw))
+sending_email(save_results(res=res, fileprefix='_zg_113'), 'zakupki-gov by INN', to_emails=to_emails)
 
-# root_logger = logging.getLogger('zg_113')
-# root_logger.info('='*46)
+# iteration for 104
+res = dict()
+parsing(get_inns(FILE_WITH_INNS_104))
+sending_email(save_results(res=res, fileprefix='_zg_104'), 'zakupki-gov by INN', to_emails=to_emails2)
+
+# iteration for 129
+res = dict()
+parsing(get_inns(FILE_WITH_INNS_129))
+sending_email(save_results(res=res, fileprefix='_zg_129'), 'zakupki-gov by INN', to_emails=to_emails3)
+
+root_logger = logging.getLogger('zg_tenders')
+root_logger.info('='*46)
 # create_db()
